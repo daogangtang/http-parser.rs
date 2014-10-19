ec694da3a04ea088f5e637fbc7aa8ff44ae6c8d1
Nathan Zadoks <nathan@nathan7.eu>
2014-10-19 22:52:54 +0200

    separate response parsers and request parsers

--- src/lib.rs
+++ src/lib.rs
@@ -7,36 +7,70 @@ use std::mem::uninitialized;
 use libc::{size_t, c_char};
 use handler::to_raw_settings;
 
-pub use handler::{ParserSettings, Handler};
-
-pub use bindings::http_parser_type as ParserType;
-pub use bindings::HTTP_REQUEST as Request;
-pub use bindings::HTTP_RESPONSE as Response;
-pub use bindings::HTTP_BOTH as Both;
+pub use handler::{ParserSettings, RequestHandler, ResponseHandler, Handler};
 
 mod bindings;
 mod handler;
 
-pub struct HttpParser(bindings::http_parser);
+pub trait Parser {
+  fn should_keep_alive(&self) -> bool;
+}
 
-impl HttpParser {
-  pub fn new(type_: ParserType) -> HttpParser {
+pub struct RequestParser(bindings::http_parser);
+pub struct ResponseParser(bindings::http_parser);
+
+impl RequestParser {
+  pub fn new() -> RequestParser {
     unsafe {
-      let mut parser: HttpParser = uninitialized();
-      bindings::http_parser_init(&mut parser.0, type_);
+      let mut parser: RequestParser = uninitialized();
+      bindings::http_parser_init(&mut parser.0, bindings::HTTP_REQUEST);
       parser
     }
   }
 
-  pub fn execute<T>(&mut self, handler: &mut T, settings: &ParserSettings<T>, data: &[u8]) {
+  pub fn execute<T: RequestHandler>(&mut self, handler: &mut T,
+                                    settings: &ParserSettings<T>,
+                                    data: &[u8]) {
     unsafe {
       self.0.data = (handler as *mut T) as *mut ();
       bindings::http_parser_execute(&mut self.0, to_raw_settings(settings),
-                                    data.as_ptr() as *const c_char, data.len() as size_t);
+                                    data.as_ptr() as *const c_char,
+                                    data.len() as size_t);
+    }
+  }
+}
+
+impl Parser for RequestParser {
+  fn should_keep_alive(&self) -> bool {
+    unsafe {
+      bindings::http_should_keep_alive(&self.0) != 0
+    }
+  }
+}
+
+impl ResponseParser {
+  pub fn new() -> ResponseParser {
+    unsafe {
+      let mut parser: ResponseParser = uninitialized();
+      bindings::http_parser_init(&mut parser.0, bindings::HTTP_RESPONSE);
+      parser
     }
   }
 
-  pub fn should_keep_alive(&self) -> bool {
+  pub fn execute<T: ResponseHandler>(&mut self, handler: &mut T,
+                                     settings: &ParserSettings<T>,
+                                     data: &[u8]) {
+    unsafe {
+      self.0.data = (handler as *mut T) as *mut ();
+      bindings::http_parser_execute(&mut self.0, to_raw_settings(settings),
+                                    data.as_ptr() as *const c_char,
+                                    data.len() as size_t);
+    }
+  }
+}
+
+impl Parser for ResponseParser {
+  fn should_keep_alive(&self) -> bool {
     unsafe {
       bindings::http_should_keep_alive(&self.0) != 0
     }
